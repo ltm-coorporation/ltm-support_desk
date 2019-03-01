74ea1d07fc99ff9db256cfbcc3b750181a2a3783
prashantdawar <prashantdawar.2008@gmail.com>
2019-03-01 22:46:46 +0530

    update

--- src/router.js
+++ src/router.js
@@ -1,34 +1,45 @@
 //
+
 function router(req, res) {
     res.setHeader('content-type', 'application/json');
     let promise = new Promise((resolve, reject) => {
-
-
         if (req.method != 'POST') return reject({ stc: 405 });
-        return resolve(req);
+        const routeTo = req.url.split('/')[2];
+        switch (routeTo) {
+
+
+            case 'login': resolve({ message: 'login route selected' }); break;
+            default: reject({ stc: 404 });
+        }
+
+
     });
+
+
+
+
+
+
 
 
     let body = {};
     promise
         .then(resObj => {
             res.statusCode = resObj.stc || 200;
-            body = JSON.stringify(resObj);
-            res.write(body);
+            body = resObj;
         })
+
+
         .catch((err) => {
             res.statusCode = err.stc || 500;
-            body.statusCode = res.statusCode;
-            body.status = 'error occured';
-
+            body = { statusCode: res.statusCode, status: 'error occured' };
             switch (res.statusCode) {
                 case 405: body.message = 'Send request using POST'; break;
+                case 404: body.message = err.message || 'check docs for existing routes'; break;
+
                 default: body.message = err.message;
             }
-            res.write(JSON.stringify(body));
         })
-        .then(() => res.end());
+        .then(() => res.end(JSON.stringify(body)));
 }
-
-
 module.exports = { router };
